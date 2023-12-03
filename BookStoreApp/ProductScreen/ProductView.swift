cac663874069c97ff22f7df03b5a06e779318a9f
Alex <y10b@ya.ru>
2023-12-03 19:36:00 +0100

    Created navigation bar

--- BookStoreApp/ProductScreen/ProductView.swift
+++ BookStoreApp/ProductScreen/ProductView.swift
@@ -8,11 +8,53 @@
 import SwiftUI
 
 struct ProductView: View {
+
+    @Environment(\.presentationMode) var presentationMode
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .navigationTitle("Title")
+        .navigationBarBackButtonHidden(true)
+        .navigationBarItems(leading: CustomNavButton(image: "arrow.backward" , action: {
+            presentationMode.wrappedValue.dismiss()
+        }))
+        .navigationBarItems(trailing: CustomNavButton(image: "heart" , action: {
+            // save/ unsave liked
+        }))
+        .padding()
     }
 }
 
 #Preview {
     ProductView()
+}
+
+
+struct CustomNavButton: View {
+
+    @State var changeLikedState: Bool = false
+
+    let image: String
+    let action: ()->()
+
+    var body: some View {
+        Button(action: {
+            if image == "heart" { changeLikedState.toggle() }
+            action()
+        }) {
+            Image(systemName: checkImage())
+                .imageScale(.medium)
+                .font(.title)
+                .foregroundColor(.black)
+        }
+    }
+
+    func checkImage()-> String {
+        image == "heart" ? (changeLikedState ? ("heart.fill") : ("heart")): (image)
+    }
 }
