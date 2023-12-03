82e40f5d444c015d329f1ccec9dc6c42d98a2e6d
Alex Rybachev <rybachev.as@gmail.com>
2023-12-03 18:15:12 +0400

    Initial Commit

---
+++ BookStoreApp/BookStoreAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookStoreAppApp.swift
+//  BookStoreApp
+//
+//  Created by Aleksandr Rybachev on 03.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookStoreAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
