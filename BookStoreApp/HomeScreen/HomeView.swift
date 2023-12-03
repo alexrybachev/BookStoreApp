3dabfaf613d56e3151f4b0b2526b31370c6c6cea
Alex Rybachev <rybachev.as@gmail.com>
2023-12-03 18:43:37 +0400

    organise files
    organise files

---
+++ BookStoreApp/HomeScreen/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  BookStoreApp
+//
+//  Created by Aleksandr Rybachev on 03.12.2023.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
