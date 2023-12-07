121c93c494f1895f00b81039e1690a5ae3fde283
Alex Rybachev <rybachev.as@gmail.com>
2023-12-07 22:18:52 +0400

    return BookStoreAppApp

---
+++ BookStoreApp/BookStoreAppApp.swift
@@ -0,0 +1,33 @@
+//
+//  BookStoreAppApp.swift
+//  BookStoreApp
+//
+//  Created by Aleksandr Rybachev on 07.12.2023.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        return true
+    }
+}
+
+@main
+struct BookStoreAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
+    
+    var body: some Scene {
+        WindowGroup {
+            if isOnboarding {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
