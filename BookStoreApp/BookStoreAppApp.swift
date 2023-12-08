9025404daf1bafa6d7b9f36760b2ffd1abbcd2fb
Alex <y10b@ya.ru>
2023-12-08 08:18:30 +0100

    Connect VM

--- BookStoreApp/BookStoreAppApp.swift
+++ BookStoreApp/BookStoreAppApp.swift
@@ -31,6 +31,7 @@ struct BookStoreAppApp: App {
                 OnboardingView()
             } else {
                 TabBarView()
+                    .environmentObject(viewModel)
             }
         }
     }
